1b3e11e1a9541b35f3bd5d842506723c185ab63b
Janet Zhang <jzhang12@stuy.edu>
2016-12-16 12:05:29 -0500

    hi

---
+++ intro.js
@@ -0,0 +1,12 @@
+var b = getElementById("b")
+
+var f = function(){
+	var el = createElement("ol");
+	var txt = document.createTextNode("New Thing!");
+	el.appendChild(txt);
+	document.appendEle
+	var ol = document.getElementById("thelist");
+	var li = document.createElement("ol");
+}
+
+b.addEventListener("click", f());
