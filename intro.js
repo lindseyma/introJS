1731e5b221b759eda2c1ae579aaa827813501cab
Janet Zhang <jzhang12@stuy.edu>
2016-12-19 01:40:55 -0500

    ghg

--- intro.js
+++ intro.js
@@ -1,5 +1,6 @@
 var b = document.getElementById("b");
 var l = document.getElementsByTagName("li");
+var h = document.getElementById("h");
 
 var f = function(){
 	var ol = document.getElementById("thelist");
@@ -8,12 +9,26 @@ var f = function(){
 	ol.appendChild(li);
 	for(var i=0; i<l.length; i++){
 	    l[i].addEventListener("click", rm);
-	}
+		l[i].addEventListener("mouseover", console.log("mouseover"));
+		l[i].addEventListener("mouseover", replace);
+		l[i].addEventListener("mouseover", console.log("mouseover"));
+		l[i].addEventListener("mouseover", replace);
+	};
 }
 
 b.addEventListener("click", f);
+// l[0].addEventListener("mouseover", console.log("mouseover"));
+// l[0].addEventListener("mouseover", replace);
+// l[1].addEventListener("mouseover", console.log("mouseover"));
+// l[1].addEventListener("mouseover", replace);
 
 var rm = function(){
     this.remove()
 }
 
+var replace = function(){
+	h.innerHTML = this.innerHTML;
+}
+var replace2 = function(){
+	h.innerHTML = "Hello World!";
+}
