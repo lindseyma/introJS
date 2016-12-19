07cb86a7594f165b9a8d8dd843b58a19c81b1cff
lindseyma <lma2@stuy.edu>
2016-12-19 01:38:10 -0500

    things are broken

--- intro.js
+++ intro.js
@@ -1,6 +1,6 @@
 var b = document.getElementById("b");
 var l = document.getElementsByTagName("li");
-
+var ob = document.getElementById("ob");
 var f = function(){
 	var ol = document.getElementById("thelist");
 	var li = document.createElement("li");
@@ -9,11 +9,31 @@ var f = function(){
 	for(var i=0; i<l.length; i++){
 	    l[i].addEventListener("click", rm);
 	}
-}
+};
 
 b.addEventListener("click", f);
 
-var rm = function(){
-    this.remove()
-}
+for(var i=0; i<l.length; i++){
+    l[i].addEventListener("click", rm);
+    console.log("hey");
+};
 
+var rm = function(){
+    this.remove();
+    console.log(this);
+};
+
+
+var fib = function(){
+    var fi = ???.getElementsbyTagName("li");
+    var newFib = document.createElement('li');
+    if(fi.length<2){
+	newFib.innerHTML=1;
+    }
+    else{
+	newFib.innerHTML = fi(n-1) + fi(n-2);
+    }
+};
+
+ob.addEventListener("click", fib);
+	    
